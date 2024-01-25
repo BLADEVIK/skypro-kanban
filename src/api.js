bede2cf9598db82d578959472ef493b49e318e29
Viktor Pirogov <Blade-85@inbox.ru>
2024-01-25 19:16:45 +0300

    Формы в React,начало

--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
-// const token=''
-// const userURL=''
-// export async function({login,password}){
-//     const response=fetch(userURL + '/login',{
-//      method:'POST',
-//      body:JSON.stringify({
-//         login,
-//         password,
-//      })
-//     })
-//     const date
-// }
+const token='ksdfsksdfjfsdjk';
+const userUrl="https://wedev-api.sky.pro/api/user";
+
+export async function login({login,password}){
+    const response = await fetch(userURL + "/login",{
+     method:"POST",
+     body:JSON.stringify({
+        login,
+        password,
+     }),
+    });
+    const date = await response.json();
+    return date;
+}
